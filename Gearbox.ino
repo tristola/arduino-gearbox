9ad6cff8c83bad7dbd30f441e2981118ca217d77
Toni Ristola <tristola@gmail.com>
2015-05-03 10:14:46 +0300

    updated to use debounce

--- Gearbox.ino
+++ Gearbox.ino
@@ -1,40 +1,51 @@
 
 //SET UP THE CORRECT PINS
-const int gearUpPin = 22;
-const int gearDownPin = 20;
-const int changeSignal = 7;
+const int gearUpPin = 31;
+const int gearDownPin = 30;
+const int buttonPin = 32;
 
 // DELAYS
 // delay between button presses (not to send press twice)
 const int buttonListenDelay = 500;
 //when signal is sent
-const int gearChangeDelay = 500;
+const int gearChangeDelay = 2000;
 
-// 
-int state = 0;
-
-void setup() {
-  pinMode(gearUpPin, OUTPUT);      
-  pinMode(gearDownPin, OUTPUT);      
-  pinMode(changeSignal, INPUT);     
-}
-
-void loop() {
-  //Constantly listening to button
-  int buttonState = digitalRead(changeSignal);
-  if (changeSignal == HIGH) {     
-    nextState();
-    delay(buttonListenDelay); 
-  } 
-}
-
-// This is for changing from state to state
 //gear states
 // 0-> 1 first gear
 // 1-> 2 second gear
 // 2-> 3 third gear
 // 3-> 4 shift to second
 // 4-> 0  shift to zero?
+// 
+int state = 0;
+int buttonState;
+int lastButtonState = LOW;
+long lastDebounceTime = 0;  // the last time the output pin was toggled
+long debounceDelay = 50;
+
+void setup() {
+  pinMode(gearUpPin, OUTPUT);      
+  pinMode(gearDownPin, OUTPUT);      
+  pinMode(buttonPin, INPUT);     
+}
+
+
+void loop() {
+  int reading = digitalRead(buttonPin);
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+  
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != buttonState) {
+      buttonState = reading;
+      if (buttonState == HIGH) {
+        nextState();
+      }
+    }
+  }
+  lastButtonState = reading;
+}
 
 void nextState(){
   // only one code block is run
@@ -43,22 +54,22 @@ void nextState(){
     state++;
   } else if(state == 3){
     gearDown();
-    state--;    
+    state++;    
   } else if(state == 4){
     gearDown();
     gearDown();
     state=0;    
   }  
 }
+
 void gearUp() {
   digitalWrite(gearUpPin, HIGH); 
   delay(gearChangeDelay);              
-  digitalWrite(gearUpPin, LOW)
+  digitalWrite(gearUpPin, LOW);
 }
 
 void gearDown() {
   digitalWrite(gearDownPin, HIGH); 
   delay(gearChangeDelay);              
-  digitalWrite(gearDownPin, LOW)
-  
+  digitalWrite(gearDownPin, LOW);
 }

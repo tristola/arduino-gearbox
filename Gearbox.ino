6e52ed6c1d216976502c24b4303e04ee1e37abb1
Toni Ristola <tristola@gmail.com>
2015-05-03 10:27:54 +0300

    added code to use utft display

--- Gearbox.ino
+++ Gearbox.ino
@@ -1,4 +1,13 @@
 
+#include <UTFT.h>
+#include <UTouch.h>
+
+// Declare which fonts we will be using
+extern uint8_t BigFont[];
+extern uint8_t SevenSegNumFont[];
+UTFT myGLCD(ITDB24D,A5,A4,A3,A2);
+UTouch myTouch(A1,A2,A0,8,9);
+
 //SET UP THE CORRECT PINS
 const int gearUpPin = 31;
 const int gearDownPin = 30;
@@ -24,6 +33,7 @@ long lastDebounceTime = 0;  // the last time the output pin was toggled
 long debounceDelay = 50;
 
 void setup() {
+  setupDisp();
   pinMode(gearUpPin, OUTPUT);      
   pinMode(gearDownPin, OUTPUT);      
   pinMode(buttonPin, INPUT);     
@@ -45,6 +55,7 @@ void loop() {
     }
   }
   lastButtonState = reading;
+  printGear(state);
 }
 
 void nextState(){
@@ -73,3 +84,37 @@ void gearDown() {
   delay(gearChangeDelay);              
   digitalWrite(gearDownPin, LOW);
 }
+
+
+
+
+
+
+//DISPLAY
+void setupDisp()
+{
+  myGLCD.InitLCD();
+  myGLCD.clrScr();
+  myGLCD.setFont(SevenSegNumFont);
+  myTouch.InitTouch(LANDSCAPE);
+  myTouch.setPrecision(PREC_MEDIUM);
+
+
+    myGLCD.setFont(BigFont);
+    myGLCD.print("Gear", 0, 0);
+    myGLCD.setColor(0, 0, 255);
+    myGLCD.setFont(SevenSegNumFont);
+}
+
+void printGear(int gear){
+  if(state<4){
+    myGLCD.setColor(255, 255, 255);
+    myGLCD.print(String(gear),50, 20, 0);
+  } else {
+    myGLCD.setColor(255, 0, 0);
+    myGLCD.print(String(2),50, 20, 0);
+  }
+  
+}
+
+
